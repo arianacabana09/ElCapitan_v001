0ef1f901c3d5e03c221c044ce44c603b7d93f746
Ariana Cabana <acabana.0904@gmail.com>
2017-03-09 16:38:37 -0500

    modificado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
+//Ingresar nombre de usuario
+var myParam = location.search.split('nombre=')[1];
+        var saludo = document.getElementById("nom");
+        if (myParam == undefined) {
+            var nom = prompt('¿Cual es tu nombre?');
+
+            while(nom=="" || nom==null)
+            {
+              nom = prompt("Ingrese su nombre por favor");
+            }
+            saludo.textContent = nom;
+        } else {
+            saludo.textContent = myParam;
+        }
+        document.getElementById("coders").addEventListener('click',function() {
+        this.href += "?nombre="+saludo.textContent;
+      })
+//habilitar myDropdown
 function myFunction() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
